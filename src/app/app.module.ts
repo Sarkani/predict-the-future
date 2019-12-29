3b24e6b694498480a7180c05ba16b8c5d03e59e3
Modestas <modestas.tileniss@gmail.com>
2019-12-29 16:10:41 +0000

    29.12.2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { MainLeftComponent } from './main/main-left/main-left.component';
+import { MainRightComponent } from './main/main-right/main-right.component';
+import { RightContainerComponent } from './main/main-right/right-container/right-container.component';
+import { LeftContainerComponent } from './main/main-left/left-container/left-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MainLeftComponent,
+    MainRightComponent,
+    RightContainerComponent,
+    LeftContainerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/main/main-left/main-left.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-left',
+  templateUrl: './main-left.component.html',
+  styleUrls: ['./main-left.component.sass']
+})
+export class MainLeftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
